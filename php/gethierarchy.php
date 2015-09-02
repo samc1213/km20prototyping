e75516d815de791f8cf53c39f9f5132836be3e00
Sam Cohen <samc1213@gmail.com>
2015-09-02 11:38:35 -0400

    adding s3 download of hierarchy

--- php/gethierarchy.php
+++ php/gethierarchy.php
@@ -1,3 +1,16 @@
 <?php
+    require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
+    use Aws\S3\S3Client;
+
+    $bucket = getenv(S3_COREDATA_BUCKET);
+
+    $client = S3Client::factory();
+
+    $result = $client->getObject(array(
+        'Bucket' => $bucket,
+        'Key'    => 'hierarchy.json',
+        'SaveAs' => 'hierarchy.json'
+    ));
+
     echo file_get_contents('hierarchy.json');
 ?>
